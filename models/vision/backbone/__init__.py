daef361d461a31c174fa683d6d3de88183077781
krenerd <48239275+krenerd@users.noreply.github.com>
2022-05-09 20:49:59 +0900

    b: resolve ImportError

--- models/vision/backbone/__init__.py
+++ models/vision/backbone/__init__.py
@@ -1,2 +1,3 @@
 from . import convmixer, efficientnet, mixer, resnet, vit  # noqa F401
 from .resnet import ResNet, ResNet18, ResNet34, ResNet50, ResNet101  # noqa F401
+from .build import build_backbone # noqa F401
