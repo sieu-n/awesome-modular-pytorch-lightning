b: overflow when using `np.uint8` to compute intermediate image, bug where we wrongly converting rgb->bgr

main.py
@@ -66,6 +66,8 @@ class Experiment:
     def setup_dataset(
         self, dataset_cfg, transform_cfg=None, const_cfg=None, subset_to_get=None
     ):
+        print_to_end("=")
+        print("[*] Building dataset")
         if const_cfg is None:
             const_cfg = self.const_cfg
 
@@ -132,7 +134,6 @@ class Experiment:
                 plot_samples_from_dataset(
                     datasets["trn"],
                     task=self.const_cfg["task"],
-                    image_tensor_to_numpy=True,
                     unnormalize=True,
                     normalization_mean=self.const_cfg["normalization_mean"],
                     normalization_std=self.const_cfg["normalization_std"],
@@ -199,7 +200,6 @@ class Experiment:
             Returns dictionary containing the torch.utils.data.Dataset objects for each subset. If a value for `subset`
             is provided, a single dataset corresponding to the subset value is returned.
         """
-        print_to_end("-")
         # build initial dataset to read data.
         if subset is None:
             subset_types = list(dataset_cfg["dataset_subset_cfg"].keys())
@@ -298,6 +298,8 @@ class Experiment:
         dataloader_cfg,
         subset_to_get=None,
     ):
+        print_to_end("=")
+        print("[*] Creating PyTorch `DataLoader`")
         dataloaders = {}
 
         base_dataloader_cfg = dataloader_cfg["base_dataloader"]
@@ -309,20 +311,20 @@ class Experiment:
         # build dataloader for each subset and apply to the dataset object.
         for subset, dataset in it:
             # build configs.
-            print(f"[*] Creating PyTorch `DataLoader` for subset `{subset}`.")
-            dataloader_cfg = merge_config(
+            print(f"Creating `DataLoader` for subset `{subset}`.")
+            subset_dataloader_cfg = merge_config(
                 base_dataloader_cfg, dataloader_cfg.get(subset, {})
             )
             # build collate_fn
-            if "collate_fn" in dataloader_cfg:
-                dataloader_cfg["collate_fn"] = catalog.collate_fn.build(
-                    name=dataloader_cfg["collate_fn"]["name"],
-                    **dataloader_cfg["collate_fn"].get("args", {}),
+            if "collate_fn" in subset_dataloader_cfg:
+                subset_dataloader_cfg["collate_fn"] = catalog.collate_fn.build(
+                    name=subset_dataloader_cfg["collate_fn"]["name"],
+                    **subset_dataloader_cfg["collate_fn"].get("args", {}),
                 )
             # build dataloader
             dataloaders[subset] = DataLoader(
                 dataset,
-                **dataloader_cfg,
+                **subset_dataloader_cfg,
             )
         if subset_to_get is None:
             return dataloaders
@@ -342,7 +344,7 @@ class Experiment:
             assert hasattr(self, "experiment_name")
             experiment_name = self.experiment_name
         # logger
-        print_to_end("-")
+        print_to_end("=")
         logger = create_logger(
             experiment_name=self.experiment_name,
             wandb_cfg=wandb_cfg,

utils/visualization/utils.py
@@ -17,6 +17,7 @@ preprocessor_factory = {
     "convert-mmdetbbox": [MMdetDataset2Torchvision(to_xywh=False)],
 }
 
+
 def plot_sample(
     task, data, mode="save", savedir="results/example.png", label_map=None, **kwargs
 ):
@@ -40,7 +41,7 @@ def plot_samples_from_dataset(
     subplot_dim=(5, 5),
     save_to="results/samples_vis.png",
     root_dir="",
-    image_tensor_to_numpy=False,
+    image_tensor_to_numpy=True,
     unnormalize=False,
     normalization_mean=(0.5, 0.5, 0.5),
     normalization_std=(0.5, 0.5, 0.5),
@@ -78,7 +79,7 @@ def plot_samples_from_dataset(
         if unnormalize:
             data = UnNormalize(normalization_mean, normalization_std)(data)
         if image_tensor_to_numpy:
-            data["images"] = np.asarray(TF.to_pil_image(data["images"]))
+            data["images"] = data["images"].permute(1, 2, 0).numpy().astype(np.uint8)
 
         plt.subplot(w, h, i)
         plot_image = plot_sample(