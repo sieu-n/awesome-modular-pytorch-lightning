474201dc636c0c4a219a57e644a0eb7604cf8fe3
krenerd <48239275+krenerd@users.noreply.github.com>
2022-07-21 00:46:52 +0900

    f: implement `MMDetectionTrainer`

---
+++ lightning/vision/mmdetection.py
@@ -0,0 +1,35 @@
+from cmath import e
+import torch
+import torch.nn.functional as F
+from torch import nn
+try:
+    import mmdet
+except ImportError:
+    pass
+
+from lightning.base import _BaseLightningTrainer
+
+
+class MMDetectionTrainer(_BaseLightningTrainer):
+    def __init__(self, model_cfg, training_cfg, *args, **kwargs):
+        """
+        In essence, `MMDetectionTrainer` is a pytorch-lightning version of the runners in `mmcv`. The implementation
+        was especially influenced by the implementation of `EpochBasedRunner`.
+        https://mmcv.readthedocs.io/en/latest/_modules/mmcv/runner/epoch_based_runner.html
+        """
+        super().__init__(model_cfg, training_cfg, *args, **kwargs)
+        assert "mm_model" in model_cfg
+        self.MMDET_model = mmdet.models.builder.build_detector(model_cfg["mm_model"])
+        # TODO backbone wrappers so we can use more generic feature extractors.
+
+    def forward(self, x):
+        """
+        x: batch of images
+        """
+        raise NotImplementedError()
+
+    def _training_step(self, batch, batch_idx=0):
+        return self.MMDET_model.train_step(batch, optimizer=None)
+
+    def evaluate(self, batch, stage=None):
+        return self.MMDET_model.val_step(batch)
