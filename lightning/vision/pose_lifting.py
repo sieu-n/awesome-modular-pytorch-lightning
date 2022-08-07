b: fix normalization

lightning/vision/pose_lifting.py
@@ -48,7 +48,8 @@ class PoseLiftingTrainer(_BaseLightningTrainer):
 
         pred = self(x)
         reconstruction = pred["joints"]
-
+        print("----------")
+        print(reconstruction.shape)
         loss = self.loss_fn(reconstruction, y)
 
         self.log("step/train_loss", loss)
@@ -132,17 +133,10 @@ class PoseLiftingTrainer(_BaseLightningTrainer):
         # unnormalize
         batch_size = joints.size(0)
         DEVICE = joints.device
+        ndim = self.normalization_mean.ndim
 
-        normalization_mean = (
-            torch.tile(self.normalization_mean, (batch_size, 3, 1))
-            .permute(0, 2, 1)
-            .to(DEVICE)
-        )
-        normalization_std = (
-            torch.tile(self.normalization_std, (batch_size, 3, 1))
-            .permute(0, 2, 1)
-            .to(DEVICE)
-        )
+        normalization_mean = self.normalization_mean.repeat([batch_size] + [1] * ndim).to(DEVICE)
+        normalization_std = self.normalization_std.repeat([batch_size] + [1] * ndim).to(DEVICE)
 
         joints = joints * normalization_std + normalization_mean
 

models/backbone/vision/pose_lifting/single_view.py
@@ -218,4 +218,4 @@ class PoseLiftingTemporalConv(nn.Module):
         x = self.expand(x.reshape(x.size(0), self.num_joints * 2, x.size(3)))
         for block in self.res_blocks:
             x = block(x)
-        return self.shrink(x).reshape(x.size(0), self.num_joints, 3)
+        return self.shrink(x).squeeze(1)