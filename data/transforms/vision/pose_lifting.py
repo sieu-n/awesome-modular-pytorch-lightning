c17a20860d7ce26bf0eb9e40ac5646ef9934ed71
krenerd <48239275+krenerd@users.noreply.github.com>
2022-08-12 16:03:54 +0900

    b: change `list` -> `tuple`

--- data/transforms/vision/pose_lifting.py
+++ data/transforms/vision/pose_lifting.py
@@ -33,5 +33,5 @@ class Create2DProjectionTemporal(_BaseTransform):
         assert "joint_2d" not in d
         cam = d["camera"]
         orig_shape = d["temporal_joints"].shape
-        d["joint_2d"] = cam.project_to_2D(d["temporal_joints"].reshape(-1, 3))[0].reshape(orig_shape[:-1] + [2])
+        d["joint_2d"] = cam.project_to_2D(d["temporal_joints"].reshape(-1, 3))[0].reshape(orig_shape[:-1] + (2,))
         return d
