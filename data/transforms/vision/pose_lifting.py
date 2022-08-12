ceeb12ba15d89627673be8fac6670296c6295fde
krenerd <48239275+krenerd@users.noreply.github.com>
2022-08-12 16:42:20 +0900

    f: add `is_world_coord` argument to `Create2DProjection`

--- data/transforms/vision/pose_lifting.py
+++ data/transforms/vision/pose_lifting.py
@@ -17,10 +17,14 @@ class CameraToWorldCoord(_BaseTransform):
 
 
 class Create2DProjection(_BaseTransform):
+    def __init__(self, is_world_coord=True, *args, **kwargs):
+        super(Create2DProjection, self).__init__(*args, **kwargs)
+        self.is_world_coord = is_world_coord
+
     def __call__(self, d):
         assert "joint_2d" not in d
         cam = d["camera"]
-        d["joint_2d"], _, _ = cam.project_to_2D(d["joint"])
+        d["joint_2d"], _, _ = cam.project_to_2D(d["joint"], is_world_coord=self.is_world_coord)
         return d
 
 
@@ -28,10 +32,14 @@ class Create2DProjectionTemporal(_BaseTransform):
     """
     Temporal version of Create2DProjection.
     """
+    def __init__(self, is_world_coord=True, *args, **kwargs):
+        super(Create2DProjectionTemporal, self).__init__(*args, **kwargs)
+        self.is_world_coord = is_world_coord
 
     def __call__(self, d):
         assert "joint_2d" not in d
         cam = d["camera"]
         orig_shape = d["temporal_joints"].shape
-        d["joint_2d"] = cam.project_to_2D(d["temporal_joints"].reshape(-1, 3))[0].reshape(orig_shape[:-1] + (2,))
+        d["joint_2d"] = cam.project_to_2D(d["temporal_joints"].reshape(-1, 3), is_world_coord=self.is_world_coord)[0]\
+            .reshape(orig_shape[:-1] + (2,))
         return d
