b: fix invalid normalization, `Create2DProjectionTemporal` is vectorized to be faster.

data/transforms/common.py
@@ -42,7 +42,7 @@ class ToTensor(_KeyTransform):
     def transform(self, t):
         t = torch.tensor(t, dtype=self.dtype)
         if self.channel_axis is not None:
-            dim_order = self.channel_axis + list(set(range(5)) - set(self.channel_axis))
+            dim_order = self.channel_axis + list(set(range(t.ndim)) - set(self.channel_axis))
             t = torch.permute(t, dim_order)
         return t
 

data/transforms/vision/pose_lifting.py
@@ -1,6 +1,4 @@
 # Camera is considered to be an instance of utils.camera.Human36Camera
-import numpy as np
-
 from ..base import _BaseTransform
 
 
@@ -34,12 +32,6 @@ class Create2DProjectionTemporal(_BaseTransform):
     def __call__(self, d):
         assert "joint_2d" not in d
         cam = d["camera"]
-        proj_2d = np.array(
-            [
-                cam.project_to_2D(d["temporal_joints"][idx])[0]
-                for idx in range(len(d["temporal_joints"]))
-            ]
-        )
-
-        d["joint_2d"] = proj_2d
+        orig_shape = d["temporal_joints"].shape
+        d["joint_2d"] = cam.project_to_2D(d["temporal_joints"].reshape(-1, 3))[0].reshape(orig_shape[:-1] + [2])
         return d